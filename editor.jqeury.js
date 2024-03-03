fa95361346c9ec48ff609299ed7aa63d3ab39b21
Shubham Sawant <shubham.sawant42@outlook.com>
2024-03-03 17:23:47 +0530

    Add Text

--- editor.jqeury.js
+++ editor.jqeury.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+
+    let isResizing = false;
+    let startPositionX, startPositionY, startWidth, startY, startHeight;
+
+    
     $("#boldButton").click(function() {
         document.execCommand('bold', false, null);
     });
@@ -10,12 +15,109 @@ $(document).ready(function() {
     $("#underline").click(function() {
         document.execCommand('underline', false, null);
     });
+
+    $('#add-image').click(function() {
+        // Create modal elements
+        var modal = $('<div class="modal fade" id="imageModal" tabindex="-1" role="dialog" aria-labelledby="imageModalLabel" aria-hidden="true">');
+        var modalDialog = $('<div class="modal-dialog" role="document">');
+        var modalContent = $('<div class="modal-content">');
+        var modalHeader = $('<div class="modal-header">').html('<h5 class="modal-title" id="imageModalLabel">Add Image</h5><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>');
+        var modalBody = $('<div class="modal-body">').html('<input type="file" id="imageInput" accept="image/*"><img id="previewImage" src="#" alt="Preview" style="display: none; max-width: 100%;">');
+        var modalFooter = $('<div class="modal-footer">').html('<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button><button type="button" class="btn btn-primary" id="saveImage">Save changes</button>');
+  
+        // Append modal elements to each other
+        modalContent.append(modalHeader, modalBody, modalFooter);
+        modalDialog.append(modalContent);
+        modal.append(modalDialog);
+  
+        // Append modal to body
+        $('body').append(modal);
+  
+        // Show modal
+        $('#imageModal').modal('show');
+      });
+
+
+      $(document).on('click', '#saveImage', function() {
+        var input = document.getElementById('imageInput');
+      
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+    
+            reader.onload = function(e) {
+                var image = $('<img>').attr('src', e.target.result).addClass('custom-image');
+                
+                // Append image to the editable div
+                $('.note-editable').append(image);
+    
+                // Make the added image resizable
+                image.resizable({
+                    aspectRatio: true // Maintain aspect ratio while resizing
+                });
+            };
+    
+            reader.readAsDataURL(input.files[0]);
+        }
+    
+        // Close modal
+        $('#imageModal').modal('hide');
+    });
+
+
+   // Make the resizableDiv resizable
+
+
+   const resizableDiv = document.getElementById('resizableDiv');
+
+   resizableDiv.addEventListener('mousedown', function(event) {
+    if (event.target === resizableDiv) {
+        event.preventDefault();
+        let startX = event.clientX;
+        let startY = event.clientY;
+        let startWidth = parseInt(document.defaultView.getComputedStyle(resizableDiv).width, 10);
+        let startHeight = parseInt(document.defaultView.getComputedStyle(resizableDiv).height, 10);
+
+        document.addEventListener('mousemove', resize);
+        document.addEventListener('mouseup', stopResize);
+
+        function resize(event) {
+            resizableDiv.style.width = startWidth + event.clientX - startX + 'px';
+            resizableDiv.style.height = startHeight + event.clientY - startY + 'px';
+            event.stopPropagation(); // Prevent event propagation to the parent div
+        }
+
+        function stopResize() {
+            document.removeEventListener('mousemove', resize);
+            document.removeEventListener('mouseup', stopResize);
+        }
+    }
 });
 
-// function changeTextSize(size) {
-//     var elements = document.getElementsByClassName('text');
-//     document.querySelector('.note-editable').style.fontSize = size + 'px';
-// }
+resizableDiv.addEventListener('mousemove', function(event) {
+    const { left, top, width, height } = resizableDiv.getBoundingClientRect();
+    const resizeWidth = 15; // Width of the resizing area
+    const resizeHeight = 15; // Height of the resizing area
+
+    if (
+        event.clientX >= left + width - resizeWidth && // Mouse is within the resize width range
+        event.clientX <= left + width && // Mouse is within the width of the resizable div
+        event.clientY >= top + height - resizeHeight && // Mouse is within the resize height range
+        event.clientY <= top + height // Mouse is within the height of the resizable div
+    ) {
+        resizableDiv.style.cursor = 'se-resize'; // Set cursor to resize diagonally
+    } else {
+        resizableDiv.style.cursor = 'auto'; // Reset cursor to default
+    }
+});
+
+// Allow focusing on the editable div
+resizableDiv.addEventListener('mousedown', function(event) {
+    resizableDiv.focus();
+});
+ 
+
+});
+
 
 function changeTextSize(size) {
     var selection = window.getSelection();
